dbff1bc7f3586afd4a3f351fc9bab7fb66c7568f
Nikta <wkernelteam@gmail.ru>
2013-03-05 18:09:56 +0400

    Corrected robot 2013 config file
    Signed-off-by: Nikta <wkernelteam@gmail.ru>

--- cerebellum/include/robots/robot_2013proto.h
+++ cerebellum/include/robots/robot_2013proto.h
@@ -2,16 +2,16 @@
 #define ROBOT_2013PROTO_H
 
 /**
- * @file include/robots/robot_2013proto.h
- * @brief Configuration file for Eurobot 2013 Main robot prototype
- * @author WebConn
- * @date 10 Jan 2013
- */
+* @file include/robots/robot_2013proto.h
+* @brief Configuration file for Eurobot 2013 Main robot prototype
+* @author WebConn
+* @date 10 Jan 2013
+*/
 
 /**
- * @addgroup Chassis configuration
- * @{
- */
+* @addgroup Chassis configuration
+* @{
+*/
 
 #define CONFIG_CHASSIS_APB1 RCC_APB1Periph_TIM4
 #define CONFIG_CHASSIS_APB2 RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOD
@@ -19,13 +19,13 @@
 #define CONFIG_DIR_LEFT_FWD_PIN 4
 #define CONFIG_DIR_LEFT_FWD_GPIO GPIOB
 
-#define CONFIG_DIR_LEFT_BWD_PIN 3
+#define CONFIG_DIR_LEFT_BWD_PIN 5
 #define CONFIG_DIR_LEFT_BWD_GPIO GPIOB
 
 #define CONFIG_DIR_RIGHT_FWD_PIN 2
 #define CONFIG_DIR_RIGHT_FWD_GPIO GPIOD
 
-#define CONFIG_DIR_RIGHT_BWD_PIN 5
+#define CONFIG_DIR_RIGHT_BWD_PIN 3
 #define CONFIG_DIR_RIGHT_BWD_GPIO GPIOB
 
 #define CONFIG_PWM_LEFT_PIN 9
@@ -46,13 +46,13 @@
 // CONFIG_CHASSIS_BREAK_LEVEL_GND is not set
 
 /**
- * @}
- */
+* @}
+*/
 
 /**
- * @addgroup Encoders configuration
- * @{
- */
+* @addgroup Encoders configuration
+* @{
+*/
 
 #define CONFIG_ENC_APB1 RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM2
 #define CONFIG_ENC_APB2 RCC_APB2Periph_GPIOA
@@ -71,23 +71,23 @@
 
 #define CONFIG_ENC_LEFT_TIMER TIM2
 // CONFIG_ENC_LEFT_INV is not set
-// CONFIG_ENC_LEFT_SWAP is not set
+//#define CONFIG_ENC_LEFT_SWAP 1
 
 #define CONFIG_ENC_RIGHT_TIMER TIM3
 // CONFIG_ENC_RIGHT_INV is not set
-#define CONFIG_ENC_RIGHT_SWAP
+#define CONFIG_ENC_RIGHT_SWAP 1
 
-#define CONFIG_ENC_WHEEL_RADIUS 24.5
-#define CONFIG_ENC_RESOLUTION 3000
-
-/**
- * @}
- */
+#define CONFIG_ENC_WHEEL_RADIUS 22.5
+#define CONFIG_ENC_RESOLUTION 3000*2
 
 /**
- * @addgroup Stabilisation config
- * @{
- */
+* @}
+*/
+
+/**
+* @addgroup Stabilisation config
+* @{
+*/
 
 #define CONFIG_CHASSIS_RADIUS 93
 
@@ -95,7 +95,7 @@
 // CONFIG_STAB_ALGO_ANGLE is not set
 
 /**
- * @}
- */
+* @}
+*/
 
 #endif
