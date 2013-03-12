2863af65dd9117e91775f5baf68b329a14d44eba
Nikta <wkernelteam@gmail.ru>
2013-03-12 14:00:30 +0400

    Full LED support
    Signed-off-by: Nikta <wkernelteam@gmail.ru>

--- cerebellum/src/led.c
+++ cerebellum/src/led.c
@@ -1,40 +1,63 @@
 #include <stm32f10x.h>
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
-
-#define LED_RCC RCC_APB2Periph_GPIOA
-#define LED_GPIO GPIOA
-#define LED_PIN 5
+#include <robots/config.h>
 
 void led_init(void)
 {
     // enable clock
-    RCC_APB2PeriphClockCmd(LED_RCC, ENABLE);
+    RCC_APB2PeriphClockCmd(CONFIG_LED1_RCC | CONFIG_LED2_RCC | CONFIG_LED3_RCC, ENABLE);
 
     // init gpio
     GPIO_InitTypeDef led = {
         .GPIO_Mode = GPIO_Mode_Out_PP,
         .GPIO_Speed = GPIO_Speed_2MHz,
-        .GPIO_Pin = (1<<LED_PIN)
     };
 
-    GPIO_Init(LED_GPIO, &led);
+    led.GPIO_Pin = (1<<CONFIG_LED1_PIN);
+    GPIO_Init(CONFIG_LED1_GPIO, &led);
+
+    led.GPIO_Pin = (1<<CONFIG_LED2_PIN);
+    GPIO_Init(CONFIG_LED2_GPIO, &led);
+
+    led.GPIO_Pin = (1<<CONFIG_LED3_PIN);
+    GPIO_Init(CONFIG_LED3_GPIO, &led);
 }
 
-void led_on(void)
+void led_on(uint32_t led)
 {
-    GPIO_SetBits(LED_GPIO, (1<<LED_PIN));
+    switch(led)
+    {
+        case 1:
+            GPIO_SetBits(CONFIG_LED1_GPIO, (1<<CONFIG_LED1_PIN));
+            break;
+        case 2:
+            GPIO_SetBits(CONFIG_LED2_GPIO, (1<<CONFIG_LED2_PIN));
+            break;
+        case 3:
+            GPIO_SetBits(CONFIG_LED3_GPIO, (1<<CONFIG_LED3_PIN));
+    }
 }
 
-void led_off(void)
+void led_off(uint32_t led)
 {
-    GPIO_ResetBits(LED_GPIO, (1<<LED_PIN));
+    switch(led)
+    {
+        case 1:
+            GPIO_ResetBits(CONFIG_LED1_GPIO, (1<<CONFIG_LED1_PIN));
+            break;
+        case 2:
+            GPIO_ResetBits(CONFIG_LED2_GPIO, (1<<CONFIG_LED2_PIN));
+            break;
+        case 3:
+            GPIO_ResetBits(CONFIG_LED3_GPIO, (1<<CONFIG_LED3_PIN));
+    }
 }
 
-void led_write(int val)
+void led_write(int led, int val)
 {
     if(val)
-        led_on();
+        led_on(led);
     else
-        led_off();
+        led_off(led);
 }
