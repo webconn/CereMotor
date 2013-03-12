28c6f7579666def7476ad93ca87225c96b02d20b
Nikta <wkernelteam@gmail.ru>
2013-03-12 14:01:22 +0400

    Add angle and path correcting constants
    Signed-off-by: Nikta <wkernelteam@gmail.ru>

--- cerebellum/include/cerebellum/robot.h
+++ cerebellum/include/cerebellum/robot.h
@@ -8,7 +8,7 @@
  * @addgroup Conversions
  */
 
-#define mmToTicks(m) ((uint32_t) ((m*CONFIG_ENC_RESOLUTION)/(2*CONFIG_ENC_WHEEL_RADIUS*M_PI)))
+#define mmToTicks(m) ((uint32_t) ((m*CONFIG_ENC_RESOLUTION*CONFIG_PATH_RCOEFF)/(2*CONFIG_ENC_WHEEL_RADIUS*M_PI)))
 #define getChassisRadius() mmToTicks(CONFIG_CHASSIS_RADIUS)
 
 #endif
