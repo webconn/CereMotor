3f5a8b8c1b188de14424cf51018a4d18cc56173a
Maslov Nikita <wkernelteam@gmail.ru>
2013-03-19 20:36:35 +0400

    Add Servo library
    Ported from PeriphBoard
    
    Signed-off-by: Maslov Nikita <wkernelteam@gmail.ru>

---
+++ cerebellum/include/cerebellum/servo.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_CEREBELLUM_H
+#define INCLUDE_CEREBELLUM_H
+
+#include <stm32f10x.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_tim.h>
+#include <misc.h>
+#include <cerebellum/snippet.h>
+#include <robots/config.h>
+
+typedef uint32_t servo;
+
+void servo_init(void);
+servo servo_add(GPIO_TypeDef * gpio, uint8_t pin);
+void servo_write(servo servo_id, uint16_t value);
+
+#endif
